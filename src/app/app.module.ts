c69d78d1da799326fc632a0c765569e26e4a904f
shartm123 <79834340+shartm123@users.noreply.github.com>
2022-05-18 11:39:11 +0200

    added  post detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostDetail } from './posts/detail/post-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostListComponent
+    PostListComponent,
+    PostDetail
   ],
   imports: [
     BrowserModule,
---
+++ src/app/posts/detail/post-detail.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {BlogService} from "../../services/blog.service";
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './post-detail.component.html',
+  styleUrls: ['./post-detail.component.scss']
+})
+export class PostDetail implements OnInit {
+  singlePosts: any;
+  errorMessage: any;
+  id!: string
+
+  constructor(
+    private blogService: BlogService,
+    private route: ActivatedRoute
+  ) { }
+
+
+  ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id') as string;
+    this.getSinglePosts()
+  }
+  getSinglePosts() {
+    this.blogService.getSinglePosts(this.id).subscribe(
+      data => {
+        this.singlePosts = data;
+      },
+      (error) => {
+        this.errorMessage = error.message
+        console.log(error);
+      });
+  }}
