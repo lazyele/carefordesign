87fdf850d09bfd51a2f6e19e3383ef58ca8a89ad
shartm123 <79834340+shartm123@users.noreply.github.com>
2022-05-23 18:32:33 +0200

    Revert "Cache added"
    This reverts commit 4bde582cc042865b357654dc01f5fd9582ef4a72.

--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -1,31 +1,22 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {IPost} from "../dto/IPost";
-import {Observable, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogService {
   private static readonly blogUrl = 'https://www.carefordesign.ch'
-  private cache: IPost[] =[];
 
   constructor(private http: HttpClient) {
   }
 
-  getPosts() : Observable<IPost[]>{
-    if(this.cache.length !== 0){
-      return of(this.cache);
-    }
-
+  getPosts() {
     const url = `${BlogService.blogUrl}/wp-json/wp/v2/posts`;
     return this.http.get<IPost[]>(url);
   }
 
-  getPost(id: number): Observable<IPost> {
-    const cachedPost = this.cache.filter(p => p.id == id);
-    if(cachedPost.length === 1)
-      return  of(cachedPost[0]);
+  getPost(id: number) {
     const url = `${BlogService.blogUrl}/wp-json/wp/v2/posts/${id}`;
     return this.http.get<IPost>(url);
   }
