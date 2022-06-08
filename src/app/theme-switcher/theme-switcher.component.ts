b66e8ad58f0263daf3eca3dc5a3363243995888a
shartm123 <79834340+shartm123@users.noreply.github.com>
2022-06-08 22:12:52 +0200

    Themeswitcher implemented

--- src/app/theme-switcher/theme-switcher.component.ts
+++ src/app/theme-switcher/theme-switcher.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {IThemeOption, ThemeService} from "../services/theme.service";
 
 @Component({
   selector: 'app-theme-switcher',
@@ -6,11 +7,18 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./theme-switcher.component.scss']
 })
 export class ThemeSwitcherComponent implements OnInit {
+  themes: IThemeOption[] = [];
 
-  constructor() {
+  constructor(private readonly themeService: ThemeService) {
   }
 
   ngOnInit(): void {
+    this.themeService.getThemes()
+      .subscribe({next: themes => this.themes = themes});
   }
 
+  changeTheme(theme: IThemeOption) {
+    this.themeService.setTheme(theme.value)
+
+  }
 }
