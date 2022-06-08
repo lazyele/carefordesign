683d7dd3749551f35cec15c083589db9da17a463
shartm123 <79834340+shartm123@users.noreply.github.com>
2022-06-08 21:47:33 +0200

    Theme integrated in theme.service.ts

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable, of} from "rxjs";
+import {StylesheetService} from "./stylesheet.service";
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +9,23 @@ import {Observable, of} from "rxjs";
 export class ThemeService {
 
   constructor(
-    private readonly httpClient: HttpClient
-  ) { }
+    private readonly httpClient: HttpClient,
+    private readonly styleSheet: StylesheetService
+  ) {
+  }
 
-  getThemes(): Observable<IThemeOption[]>{
+  getThemes(): Observable<IThemeOption[]> {
     return of(themes);
   }
-  //setTheme()
+
+  setTheme(themeToSet: string) {
+    this.styleSheet.setStyle(
+      "theme",
+      `node_modules/@angular/material/prebuilt-themes/${themeToSet}.css`
+    );
+  }
 }
+
 export interface IThemeOption {
   backgroundColor: string;
   buttonColor: string;
@@ -23,34 +33,35 @@ export interface IThemeOption {
   label: string;
   value: string;
 }
-export const themes:IThemeOption[]=
-[
-  {
-    "backgroundColor": "#fff",
-    "buttonColor": "#ffc107",
-    "headingColor": "#673ab7",
-    "label": "Deep Purple & Amber",
-    "value": "deeppurple-amber"
-  },
-  {
-    "backgroundColor": "#fff",
-    "buttonColor": "#ff4081",
-    "headingColor": "#3f51b5",
-    "label": "Indigo & Pink",
-    "value": "indigo-pink"
-  },
-  {
-    "backgroundColor": "#303030",
-    "buttonColor": "#607d8b",
-    "headingColor": "#e91e63",
-    "label": "Pink & Blue Grey",
-    "value": "pink-bluegrey"
-  },
-  {
-    "backgroundColor": "#303030",
-    "buttonColor": "#4caf50",
-    "headingColor": "#9c27b0",
-    "label": "Purple & Green",
-    "value": "purple-green"
-  }
-]
+
+export const themes: IThemeOption[] =
+  [
+    {
+      "backgroundColor": "#fff",
+      "buttonColor": "#ffc107",
+      "headingColor": "#673ab7",
+      "label": "Deep Purple & Amber",
+      "value": "deeppurple-amber"
+    },
+    {
+      "backgroundColor": "#fff",
+      "buttonColor": "#ff4081",
+      "headingColor": "#3f51b5",
+      "label": "Indigo & Pink",
+      "value": "indigo-pink"
+    },
+    {
+      "backgroundColor": "#303030",
+      "buttonColor": "#607d8b",
+      "headingColor": "#e91e63",
+      "label": "Pink & Blue Grey",
+      "value": "pink-bluegrey"
+    },
+    {
+      "backgroundColor": "#303030",
+      "buttonColor": "#4caf50",
+      "headingColor": "#9c27b0",
+      "label": "Purple & Green",
+      "value": "purple-green"
+    }
+  ]
