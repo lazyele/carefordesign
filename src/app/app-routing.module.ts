95940cb8b82e454d9f2b26359118d49205ac8c42
shartm123 <79834340+shartm123@users.noreply.github.com>
2022-07-05 18:13:59 +0200

    Page Components moved to Pages-Folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {PostListComponent} from "./posts/post-list/post-list.component";
 import {PostDetailComponent} from "./posts/detail/post-detail.component";
-import {AboutUsComponent} from "./about-us/about-us.component";
-import {HomeComponent} from "./home/home.component";
-import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {AboutUsComponent} from "./pages/about-us/about-us.component";
+import {HomeComponent} from "./pages/home/home.component";
+import {PageNotFoundComponent} from "./pages/page-not-found/page-not-found.component";
 
 const routes: Routes = [
   {path: 'posts', component: PostListComponent},
