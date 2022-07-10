ed88ecc13e7ffde6f01d97361fc33c093ae55515
sesea <sesea.hartmann@stud.fhgr.ch>
2022-07-10 16:04:28 +0200

    Search function added

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
 import {IPost} from "../dto/IPost";
 import {BlogFilterPipe} from "../filter/blog-filter.pipe";
 import {Router} from "@angular/router";
@@ -9,26 +9,31 @@ import {Router} from "@angular/router";
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
- @Input() posts: IPost[] = [];
- filteredPosts: IPost[] = [];
- searchInput = ""
-  constructor(private readonly blogFilterPipe:BlogFilterPipe, private readonly router:Router) {  }
+  @Input() posts: IPost[] = [];
+  filteredPosts: IPost[] = [];
+  searchInput = ""
+  hasNoItems = false;
+
+  constructor(private readonly blogFilterPipe: BlogFilterPipe, private readonly router: Router) {
+  }
 
   ngOnInit(): void {
-   this.filteredPosts = this.posts;
+    this.filteredPosts = this.posts;
+
   }
 
   onKeyUp() {
-   this.filteredPosts = this.blogFilterPipe.transform(this.posts, this.searchInput);
+    this.filteredPosts = this.blogFilterPipe.transform(this.posts, this.searchInput);
+    this.hasNoItems = this.filteredPosts.length===0;
 
   }
 
   onSubmit() {
-   if(this.filteredPosts.length===1){
-    this.router.navigate(["posts", this.filteredPosts[0].id])
-   }
-   else {
+    if (this.filteredPosts.length === 1) {
+      this.router.navigate(["posts", this.filteredPosts[0].id])
+    } else {
 
-   }
+    }
   }
+
 }
