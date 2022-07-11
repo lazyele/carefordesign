6a59fd2ca3e2088a33ec723648e7e6d6c186e80a
sesea <sesea.hartmann@stud.fhgr.ch>
2022-07-11 13:30:18 +0200

    Post Image and Spinner added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import {MatListModule} from "@angular/material/list";
 import {MatInputModule} from "@angular/material/input";
 import {MatCardModule} from "@angular/material/card";
 import {MatChipsModule} from "@angular/material/chips";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 @NgModule({
   declarations: [
@@ -63,7 +64,8 @@ import {MatChipsModule} from "@angular/material/chips";
     MatListModule,
     MatInputModule,
     MatCardModule,
-    MatChipsModule
+    MatChipsModule,
+    MatProgressSpinnerModule
 
   ],
   providers: [HttpClient, BlogFilterPipe],
