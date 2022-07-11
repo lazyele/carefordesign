a24bb45495028e9ebcae789396de61f5c431b0ea
sesea <sesea.hartmann@stud.fhgr.ch>
2022-07-11 22:30:05 +0200

    bug fixing

--- src/app/filters/tag-filter.pipe.ts
+++ src/app/filters/tag-filter.pipe.ts
@@ -14,10 +14,14 @@ export class TagFilterPipe implements PipeTransform {
       console.log(posts, searchTagIds)
       return posts;
     }
-    const results = posts.filter(p =>
-      p.tags.some(t => searchTagIds.find(t))
-    )
-    console.log(results);
+    const results = posts.filter(p => {
+        // todo filtering
+        // happy coding !
+        const r = p.tags.some(t => searchTagIds.some(st => st === t.id));
+        console.log(p.tags, r, searchTagIds);
+      }
+    );
+    console.log(results.length);
     return results;
   }
 
