ecb18105d4975efa08d0c92773f9b110d5584d2e
sesea <sesea.hartmann@stud.fhgr.ch>
2022-07-11 13:29:51 +0200

    Post Image and Spinner added

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -2,7 +2,8 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {BlogService} from "../../services/blog.service";
 import {ActivatedRoute} from "@angular/router";
 import {IPost} from "../../dto/IPost";
-import {Subscription} from "rxjs";
+import {Subscription, tap} from "rxjs";
+import {IMedia} from "../../dto/IMedia";
 
 @Component({
   selector: 'app-post-list',
@@ -11,6 +12,7 @@ import {Subscription} from "rxjs";
 })
 export class PostListComponent implements OnInit, OnDestroy {
   blogPosts: IPost[] = [];
+  images = new Map<number, IMedia>()
   searchInput = "";
   searchInputSubscription!: Subscription;
 
@@ -31,9 +33,30 @@ export class PostListComponent implements OnInit, OnDestroy {
     this.blogService.getPosts()
       .subscribe(
         {
-          next: data => this.blogPosts = data,
+          next: data => {
+            this.blogPosts = data
+            this.loadImages(this.blogPosts);
+          },
           error: error => console.log(error)
         });
+
+  }
+
+  private loadImages(posts: IPost[]) {
+    posts.filter(p => !this.images.has(p.featured_media))
+      .forEach(p => this.blogService.getImage(p.featured_media)
+        .subscribe(p => {
+          console.log(p)
+          if (this.images.has(p.id)) {
+            return;
+          }
+          this.images.set(p.id, p);
+        })
+      );
+  }
+
+  getImage(id: number) {
+    return this.blogService.getImage(id).pipe(tap(i => console.log(i)))
   }
 
   ngOnDestroy(): void {
