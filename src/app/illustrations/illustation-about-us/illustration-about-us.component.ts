507105382547a7340eaa2d900bd8a40ca65dae10
sesea <sesea.hartmann@stud.fhgr.ch>
2022-07-14 10:51:15 +0200

    About Us illustration edited

---
+++ src/app/illustrations/illustation-about-us/illustration-about-us.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-illustation-about-us',
+  templateUrl: './illustration-about-us.component.html',
+  styleUrls: ['./illustration-about-us.component.scss']
+})
+export class IllustrationAboutUsComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
