d1d83fbd4724761508bcd9bde82dfd519a11e9ad
sesea <sesea.hartmann@stud.fhgr.ch>
2022-07-14 15:25:18 +0200

    project illustration files generated

---
+++ src/app/illustrations/illustration-project/illustration-project.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-illustration-project',
+  templateUrl: './illustration-project.component.html',
+  styleUrls: ['./illustration-project.component.scss']
+})
+export class IllustrationProjectComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
+
