02e457ee7fdf0dc8a957674031f422bd0367fae2
sesea <sesea.hartmann@stud.fhgr.ch>
2022-07-15 22:12:53 +0200

    post-list is more readable

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -10,7 +10,7 @@ import {IMedia} from "../../dto/IMedia";
 })
 export class PostListComponent implements OnInit {
   @Input() posts: IPost[] = [];
-  images = new Map<number, IMedia>()
+  private images = new Map<number, IMedia>()
   isImageLoading = false;
 
   constructor(
@@ -22,9 +22,21 @@ export class PostListComponent implements OnInit {
     this.loadImages(this.posts);
   }
 
+  hasImage(post: IPost) {
+    return this.images.has(post.featured_media);
+  }
+
+  getImageSource(post: IPost) {
+    return this.images?.get(post.featured_media)
+      ?.media_details?.sizes?.medium?.source_url ?? ''
+  }
+
+  getImageAlt(post: IPost) {
+    return this.images?.get(post.featured_media)?.alt_text ?? ''
+  }
+
   private loadImages(posts: IPost[]) {
     posts
-      // todo on call
       .filter(p => p.featured_media !== 0)
       .filter(p => !this.images.has(p.featured_media))
       .forEach(p => this.blogService.getImage(p.featured_media)
