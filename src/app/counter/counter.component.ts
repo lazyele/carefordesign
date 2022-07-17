677f8c61bf6f45706219d0d2a1cb657b48a19015
sesea <sesea.hartmann@stud.fhgr.ch>
2022-07-17 07:58:24 +0200

    counter componente

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -6,6 +6,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./counter.component.scss']
 })
 export class CounterComponent implements OnInit {
+  @Input() title = '';
+  @Input() count?: number;
 
   constructor() {
   }
