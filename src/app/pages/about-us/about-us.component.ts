071947ef5ef3bc3ed85ee6543428e1dceaa151a7
sesea <sesea.hartmann@stud.fhgr.ch>
2022-07-21 10:06:32 +0200

    About Us Protrait Content

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,16 +1,17 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {AboutUsItems} from "../../../environments/about-us-items";
 
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss']
 })
-export class AboutUsComponent implements OnInit {
+export class AboutUsComponent {
+
+  aboutUsItems = AboutUsItems
 
   constructor() {
   }
 
-  ngOnInit(): void {
-  }
 
 }
