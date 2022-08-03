e9e1b505a97f0d3f2929e42a6bf297ffbed6e7c1
sesea <sesea.hartmann@stud.fhgr.ch>
2022-08-03 06:56:42 +0200

    basic set up local storage

--- src/app/services/theme/localstorage.service.ts
+++ src/app/services/theme/localstorage.service.ts
@@ -1,3 +1,26 @@
-localStorage.setItem("string", "any");
+import {Injectable} from '@angular/core';
 
-localStorage.getItem("string");
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  constructor() {
+  }
+
+  public saveData(key: string, value: string) {
+    localStorage.setItem(key, value);
+  }
+
+  public getData(key: string) {
+    return localStorage.getItem(key)
+  }
+
+  public removeData(key: string) {
+    localStorage.removeItem(key);
+  }
+
+  public clearData() {
+    localStorage.clear();
+  }
+}
--- src/app/services/theme/theme.service.ts
+++ src/app/services/theme/theme.service.ts
@@ -4,6 +4,8 @@ import {BehaviorSubject, Observable, of} from "rxjs";
 import {StylesheetService} from "./stylesheet.service";
 import {environment} from "../../../environments/environment";
 import {ThemeType} from "../../../environments/themes";
+import {LocalStorageService} from "./localstorage.service";
+
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +16,8 @@ export class ThemeService {
 
   constructor(
     private readonly httpClient: HttpClient,
-    private readonly styleSheet: StylesheetService
+    private readonly styleSheet: StylesheetService,
+    private localStorage: LocalStorageService
   ) {
   }
 
@@ -28,7 +31,10 @@ export class ThemeService {
       `/assets/themes/${theme}.css`
     );
     this.currentTheme$.next(this.getTheme(theme));
+    this.localStorage.saveData("theme", theme);
+    console.log(localStorage);
   }
+
 
   private getTheme(theme: ThemeType): IThemeOption {
     return environment.themes.filter(t => t.type == theme)[0];
