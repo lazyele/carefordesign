991369e637a97c452f40c8d7c5cd721c81c750fb
sesea <sesea.hartmann@stud.fhgr.ch>
2022-08-10 15:02:29 +0200

    videos added to environment

---
+++ src/environments/accessibility-video.ts
@@ -0,0 +1,12 @@
+export const AccessibilityVideoItems =
+  [
+    {
+      title: "Simulation Light-Theme",
+      href: "Simulation_Lighttheme.mp4"
+    },
+    {
+      title: "Simulation Dark-Theme",
+      href: "Simulation_Darktheme.mp4"
+    },
+  ]
+;
